6175df8bb9fe9de450f4299d23c31a4f8a47156c
Windows 10 <moh.nabil4444@gmail.com>
2024-01-31 22:12:02 +0200

    intial project added home screen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:islame_project/home/home_screen.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Home_Screen.routeName,
+      routes: {
+        Home_Screen.routeName: (context) => Home_Screen(),
+      },
+    );
+  }
+}
