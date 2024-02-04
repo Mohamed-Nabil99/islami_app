1a51903ee0a9c4d726ab172be126982745127dcb
Windows 10 <moh.nabil4444@gmail.com>
2024-02-05 01:29:10 +0200

    ended application light mode

--- lib/home/radio/radio_tab.dart
+++ lib/home/radio/radio_tab.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:islame_project/my_theme.dart';
 
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(bottom: 20), // Adjust padding as needed
+          child: Column(
+            children: [
+              Image.asset('assets/images/radio_image.png'), // Radio image
+              SizedBox(height: 10), // Add space between image and text
+              Text(
+                'إذاعة القرآن الكريم',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+            ],
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              color: MyTheme.primaryLight,
+              iconSize: 50,
+              icon: Icon(Icons.skip_previous), // Back icon
+              onPressed: () {
+                // Add your onPressed logic here
+              },
+            ),
+            IconButton(
+              color: MyTheme.primaryLight,
+              iconSize: 50,
+              icon: Icon(Icons.play_arrow), // Play icon
+              onPressed: () {
+                // Add your onPressed logic here
+              },
+            ),
+            IconButton(
+              color: MyTheme.primaryLight,
+              iconSize: 50,
+              icon: Icon(Icons.skip_next), // Play next icon
+              onPressed: () {
+                // Add your onPressed logic here
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/home/sebha/sebha_tab.dart
+++ lib/home/sebha/sebha_tab.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
+  @override
+  _SebhaTabState createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int numberOfTasbeeh = 0;
+  List<String> tasbeehPhrases = ['سبحان الله', 'الحمد لله', 'الله أكبر'];
+  int currentTasbeehIndex = 0;
+
+  BoxDecoration myBoxDecoration() {
+    return BoxDecoration(
+      color: Color(0xffB7935F),
+      borderRadius: BorderRadius.all(
+        Radius.circular(25),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.orange,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 100),
+                child: Image.asset('assets/images/head_sebha_logo.png'),
+              ),
+            ],
+          ),
+          InkWell(
+            onTap: () {
+              setState(() {
+                numberOfTasbeeh = (numberOfTasbeeh + 1) % 34;
+                if (numberOfTasbeeh == 0) {
+                  currentTasbeehIndex =
+                      (currentTasbeehIndex + 1) % tasbeehPhrases.length;
+                }
+              });
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Image.asset('assets/images/body_sebha_logo.png'),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Text(
+                  'عدد التسبيحات',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                SizedBox(height: 15),
+                Container(
+                  width: 70,
+                  height: 65,
+                  decoration: myBoxDecoration(),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Text(
+                      '$numberOfTasbeeh',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                            color: Colors.black,
+                            fontFamily: 'Monotype Koufi',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  width: 163,
+                  height: 55,
+                  decoration: myBoxDecoration(),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Text(
+                      tasbeehPhrases[currentTasbeehIndex],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Monotype Koufi',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
